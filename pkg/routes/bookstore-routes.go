2df2d2fae24407c9698157c1f80cafb514ddbde0
Lakshmi Prasad <lakshmi.prasad@MACPRO511.local>
2023-11-24 17:56:31 +0530

    chnage

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,10 +5,19 @@ import (
 	"github.com/prasaddls/go-bookmicroservice/pkg/controllers"
 )
 
-var RegisterBookStoreRoutes = func(route *mux.Route) {
-	route.HandlerFunc("/books", controllers.CreateBook).Methods("POST")
-	route.HandlerFunc("/books", controllers.GetBooks).Methods("GET")
-	route.HandlerFunc("/books/{id}", controllers.GetBook).Methods("GET")
-	route.HandlerFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
-	route.HandlerFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	// Create book
+	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
+
+	// Get all books
+	router.HandleFunc("/books", controllers.GetBook).Methods("GET")
+
+	// Get a specific book by ID
+	router.HandleFunc("/books/{id}", controllers.GetBook).Methods("GET")
+
+	// Update a specific book by ID
+	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
+
+	// Delete a specific book by ID
+	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
 }
